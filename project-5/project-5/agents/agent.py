52cc9843c58fee5c709f189c9f96ca9eea7dbc1a
Van Chen <vanchen523@qq.com>
2018-08-05 01:18:19 +0800

    第二次提交项目
    1.修改错误代码（神经网络部分）。2.去掉bn和dropout层。3.调整超参数。4.修改奖励函数

--- project-5/project-5/agents/agent.py
+++ project-5/project-5/agents/agent.py
@@ -27,25 +27,25 @@ class DDPG():
 
         # Noise process
         self.exploration_mu = 0
-        self.exploration_theta = 0.15
-        self.exploration_sigma = 0.2
+        self.exploration_theta = 0.1
+        self.exploration_sigma = 0.0002
         self.noise = OUNoise(self.action_size, self.exploration_mu, self.exploration_theta, self.exploration_sigma)
 
         # Replay memory
         self.buffer_size = 100000
-        self.batch_size = 128
+        self.batch_size = 32
         self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
 
         # Algorithm parameters
         self.gamma = 0.99  # discount factor
         self.tau = 0.001  # for soft update of target parameters
-        
-        self.score = 0
+
+        self.score = 0.0
         self.best_score = -np.inf
         self.count = 0
-
+        
     def reset_episode(self):
-        self.total_reward = .0
+        self.total_reward = 0.0
         self.count = 0
         self.noise.reset()
         state = self.task.reset()
@@ -53,10 +53,10 @@ class DDPG():
         return state
 
     def step(self, action, reward, next_state, done):
-        
         self.total_reward += reward
         self.count += 1
-         # Save experience / reward
+        
+        # Save experience / reward
         self.memory.add(self.last_state, action, reward, next_state, done)
 
         # Learn, if enough samples are available in memory
@@ -67,10 +67,10 @@ class DDPG():
         # Roll over last state and action
         self.last_state = next_state
 
-    def act(self, states):
+    def act(self, state):
         """Returns actions for given state(s) as per current policy."""
-        states = np.reshape(states, [-1, self.state_size])
-        action = self.actor_local.model.predict(states)[0]
+        state = np.reshape(state, [-1, self.state_size])
+        action = self.actor_local.model.predict(state)[0]
         return list(action + self.noise.sample())  # add some noise for exploration
 
     def learn(self, experiences):
@@ -111,7 +111,7 @@ class DDPG():
         assert len(local_weights) == len(target_weights), "Local and target model parameters must have the same size"
 
         new_weights = self.tau * local_weights + (1 - self.tau) * target_weights
-        target_model.set_weights(new_weights)       
+        target_model.set_weights(new_weights)
         
 import copy
 class OUNoise:
@@ -192,14 +192,15 @@ class Actor:
 
         # Add hidden layers
         net = layers.Dense(units=32, activation='relu')(states)
-        net = layers.BatchNormalization()(net)
-        net = layers.Dropout(0.4)(net)
+#         net = layers.BatchNormalization()(net)
+#         net = layers.Dropout(0.4)(net)
         net = layers.Dense(units=64, activation='relu')(net)
-        net = layers.BatchNormalization()(net)
-        net = layers.Dropout(0.4)(net)
+#         net = layers.BatchNormalization()(net)
+#         net = layers.Dropout(0.4)(net)
         net = layers.Dense(units=32, activation='relu')(net)
-        net = layers.BatchNormalization()(net)
-        net = layers.Dropout(0.4)(net)
+#         net = layers.BatchNormalization()(net)
+#         net = layers.Dropout(0.4)(net)
+        
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 
         # Add final output layer with sigmoid activation
@@ -220,7 +221,7 @@ class Actor:
         # Incorporate any additional losses here (e.g. from regularizers)
 
         # Define optimizer and training function
-        optimizer = optimizers.Adam()
+        optimizer = optimizers.Adam(0.0005)
         updates_op = optimizer.get_updates(params=self.model.trainable_weights, loss=loss)
         self.train_fn = K.function(
             inputs=[self.model.input, action_gradients, K.learning_phase()],
@@ -252,24 +253,22 @@ class Critic:
         actions = layers.Input(shape=(self.action_size,), name='actions')
 
         # Add hidden layer(s) for state pathway
-
         net_states = layers.Dense(units=32, activation='relu')(states)
-        net_states = layers.BatchNormalization(net_states)
-        net_states = layers.Dropout(0.4)(net_states)
+#         net_states = layers.BatchNormalization()(net_states)
+#         net_states = layers.Dropout(0.4)(net_states)
 
         net_states = layers.Dense(units=64, activation='relu')(net_states)
-        net_states = layers.BatchNormalization(net_states)
-        net_states = layers.Dropout(0.4)(net_states)
+#         net_states = layers.BatchNormalization()(net_states)
+#         net_states = layers.Dropout(0.4)(net_states)
         
         # Add hidden layer(s) for action pathway
-
         net_actions = layers.Dense(units=32, activation='relu')(actions)
-        net_states = layers.BatchNormalization(net_states)
-        net_states = layers.Dropout(0.4)(net_states)
+#         net_actions = layers.BatchNormalization()(net_actions)
+#         net_actions = layers.Dropout(0.4)(net_actions)
         
         net_actions = layers.Dense(units=64, activation='relu')(net_actions)
-        net_states = layers.BatchNormalization(net_states)
-        net_states = layers.Dropout(0.4)(net_states)
+#         net_actions = layers.BatchNormalization()(net_actions)
+#         net_actions = layers.Dropout(0.4)(net_actions)
         
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 
@@ -286,7 +285,7 @@ class Critic:
         self.model = models.Model(inputs=[states, actions], outputs=Q_values)
 
         # Define optimizer and compile model for training with built-in loss function
-        optimizer = optimizers.Adam()
+        optimizer = optimizers.Adam(0.005)
         self.model.compile(optimizer=optimizer, loss='mse')
 
         # Compute action gradients (derivative of Q values w.r.t. to actions)
